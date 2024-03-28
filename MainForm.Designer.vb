9dada92aed7d888eaa42ca3b7113577af23df86a
APIBox <LinkerVB@outlook.com>
2024-03-28 19:23:29 +0800

    _
    _

--- MainForm.Designer.vb
+++ MainForm.Designer.vb
@@ -26,9 +26,19 @@ Partial Class MainForm
         Panel_Move = New Panel()
         Btn_MinWindow = New Label()
         Btn_Close = New Label()
+        CtrlPanel = New Panel()
+        Fix = New Label()
+        ScanMainPage_Btn = New PictureBox()
+        MainPage_Btn = New PictureBox()
+        Panel_isGoTo = New Panel()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        Panel1 = New Panel()
         PageCtrl = New TabControl()
         MainPgae = New TabPage()
         MainPage_Scan = New TabPage()
+        ScanPanel = New Panel()
+        SelPathScan_Btn = New Label()
+        Show_ScanPanel = New Label()
         ScaningPage_Btn = New Label()
         Tip_0 = New Label()
         ScaningPage = New TabPage()
@@ -37,18 +47,18 @@ Partial Class MainForm
         Scan_Tip = New Label()
         ListView1 = New ListView()
         FileName = New ColumnHeader()
-        CtrlPanel = New Panel()
-        ScanMainPage_Btn = New PictureBox()
-        MainPage_Btn = New PictureBox()
-        Panel_isGoTo = New Panel()
-        FolderBrowserDialog1 = New FolderBrowserDialog()
+        修复 = New TabPage()
+        FixTaskMgr = New Label()
         Panel_Move.SuspendLayout()
-        PageCtrl.SuspendLayout()
-        MainPage_Scan.SuspendLayout()
-        ScaningPage.SuspendLayout()
         CtrlPanel.SuspendLayout()
         CType(ScanMainPage_Btn, ComponentModel.ISupportInitialize).BeginInit()
         CType(MainPage_Btn, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
+        PageCtrl.SuspendLayout()
+        MainPage_Scan.SuspendLayout()
+        ScanPanel.SuspendLayout()
+        ScaningPage.SuspendLayout()
+        修复.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel_Move
@@ -81,125 +91,9 @@ Partial Class MainForm
         Btn_Close.TabIndex = 0
         Btn_Close.Text = "×"
         ' 
-        ' PageCtrl
-        ' 
-        PageCtrl.Controls.Add(MainPgae)
-        PageCtrl.Controls.Add(MainPage_Scan)
-        PageCtrl.Controls.Add(ScaningPage)
-        PageCtrl.ItemSize = New Size(1, 25)
-        PageCtrl.Location = New Point(135, 49)
-        PageCtrl.Name = "PageCtrl"
-        PageCtrl.SelectedIndex = 0
-        PageCtrl.Size = New Size(940, 662)
-        PageCtrl.TabIndex = 3
-        ' 
-        ' MainPgae
-        ' 
-        MainPgae.BackColor = Color.LightSkyBlue
-        MainPgae.Location = New Point(4, 29)
-        MainPgae.Name = "MainPgae"
-        MainPgae.Padding = New Padding(3)
-        MainPgae.Size = New Size(932, 629)
-        MainPgae.TabIndex = 0
-        MainPgae.Text = "主界面"
-        ' 
-        ' MainPage_Scan
-        ' 
-        MainPage_Scan.BackColor = Color.LightSkyBlue
-        MainPage_Scan.Controls.Add(ScaningPage_Btn)
-        MainPage_Scan.Controls.Add(Tip_0)
-        MainPage_Scan.Location = New Point(4, 29)
-        MainPage_Scan.Name = "MainPage_Scan"
-        MainPage_Scan.Padding = New Padding(3)
-        MainPage_Scan.Size = New Size(932, 629)
-        MainPage_Scan.TabIndex = 1
-        MainPage_Scan.Text = "查杀_主界面"
-        ' 
-        ' ScaningPage_Btn
-        ' 
-        ScaningPage_Btn.BackColor = Color.Silver
-        ScaningPage_Btn.Font = New Font("黑体", 18F, FontStyle.Regular, GraphicsUnit.Point)
-        ScaningPage_Btn.Location = New Point(144, 316)
-        ScaningPage_Btn.Name = "ScaningPage_Btn"
-        ScaningPage_Btn.Size = New Size(196, 67)
-        ScaningPage_Btn.TabIndex = 2
-        ScaningPage_Btn.Text = "自定义查杀"
-        ScaningPage_Btn.TextAlign = ContentAlignment.MiddleCenter
-        ' 
-        ' Tip_0
-        ' 
-        Tip_0.AutoSize = True
-        Tip_0.Font = New Font("黑体", 36F, FontStyle.Regular, GraphicsUnit.Point)
-        Tip_0.ForeColor = SystemColors.ButtonHighlight
-        Tip_0.Location = New Point(121, 172)
-        Tip_0.Name = "Tip_0"
-        Tip_0.Size = New Size(535, 120)
-        Tip_0.TabIndex = 1
-        Tip_0.Text = "查杀木马病毒,拦截" & vbCrLf & "可疑行为"
-        ' 
-        ' ScaningPage
-        ' 
-        ScaningPage.BackColor = Color.LightSkyBlue
-        ScaningPage.Controls.Add(VirusCount)
-        ScaningPage.Controls.Add(Del)
-        ScaningPage.Controls.Add(Scan_Tip)
-        ScaningPage.Controls.Add(ListView1)
-        ScaningPage.Location = New Point(4, 29)
-        ScaningPage.Name = "ScaningPage"
-        ScaningPage.Padding = New Padding(3)
-        ScaningPage.Size = New Size(932, 629)
-        ScaningPage.TabIndex = 2
-        ScaningPage.Text = "查杀_扫描中"
-        ' 
-        ' VirusCount
-        ' 
-        VirusCount.AutoSize = True
-        VirusCount.Font = New Font("黑体", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        VirusCount.Location = New Point(6, 43)
-        VirusCount.Name = "VirusCount"
-        VirusCount.Size = New Size(99, 20)
-        VirusCount.TabIndex = 4
-        VirusCount.Text = "威胁数量:"
-        ' 
-        ' Del
-        ' 
-        Del.BackColor = Color.Silver
-        Del.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
-        Del.Location = New Point(832, 22)
-        Del.Name = "Del"
-        Del.Size = New Size(94, 41)
-        Del.TabIndex = 3
-        Del.Text = "删除"
-        Del.TextAlign = ContentAlignment.MiddleCenter
-        ' 
-        ' Scan_Tip
-        ' 
-        Scan_Tip.AutoSize = True
-        Scan_Tip.Font = New Font("黑体", 12F, FontStyle.Regular, GraphicsUnit.Point)
-        Scan_Tip.Location = New Point(6, 10)
-        Scan_Tip.Name = "Scan_Tip"
-        Scan_Tip.Size = New Size(99, 20)
-        Scan_Tip.TabIndex = 0
-        Scan_Tip.Text = "正在扫描:"
-        ' 
-        ' ListView1
-        ' 
-        ListView1.BackColor = Color.LightSkyBlue
-        ListView1.Columns.AddRange(New ColumnHeader() {FileName})
-        ListView1.Location = New Point(6, 77)
-        ListView1.Name = "ListView1"
-        ListView1.Size = New Size(920, 550)
-        ListView1.TabIndex = 1
-        ListView1.UseCompatibleStateImageBehavior = False
-        ListView1.View = View.Details
-        ' 
-        ' FileName
-        ' 
-        FileName.Text = "文件名"
-        FileName.Width = 8000
-        ' 
         ' CtrlPanel
         ' 
+        CtrlPanel.Controls.Add(Fix)
         CtrlPanel.Controls.Add(ScanMainPage_Btn)
         CtrlPanel.Controls.Add(MainPage_Btn)
         CtrlPanel.Controls.Add(Panel_isGoTo)
@@ -208,10 +102,21 @@ Partial Class MainForm
         CtrlPanel.Size = New Size(133, 674)
         CtrlPanel.TabIndex = 0
         ' 
+        ' Fix
+        ' 
+        Fix.BackColor = Color.Silver
+        Fix.Font = New Font("黑体", 13.5F, FontStyle.Regular, GraphicsUnit.Point)
+        Fix.Location = New Point(23, 148)
+        Fix.Name = "Fix"
+        Fix.Size = New Size(106, 49)
+        Fix.TabIndex = 8
+        Fix.Text = "修复"
+        Fix.TextAlign = ContentAlignment.MiddleCenter
+        ' 
         ' ScanMainPage_Btn
         ' 
         ScanMainPage_Btn.Image = My.Resources.Resources.ScanPage
-        ScanMainPage_Btn.Location = New Point(23, 91)
+        ScanMainPage_Btn.Location = New Point(24, 91)
         ScanMainPage_Btn.Name = "ScanMainPage_Btn"
         ScanMainPage_Btn.Size = New Size(106, 50)
         ScanMainPage_Btn.SizeMode = PictureBoxSizeMode.StretchImage
@@ -236,14 +141,195 @@ Partial Class MainForm
         Panel_isGoTo.Size = New Size(5, 50)
         Panel_isGoTo.TabIndex = 5
         ' 
+        ' Panel1
+        ' 
+        Panel1.Controls.Add(PageCtrl)
+        Panel1.Location = New Point(135, 43)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(948, 674)
+        Panel1.TabIndex = 4
+        ' 
+        ' PageCtrl
+        ' 
+        PageCtrl.Controls.Add(MainPgae)
+        PageCtrl.Controls.Add(MainPage_Scan)
+        PageCtrl.Controls.Add(ScaningPage)
+        PageCtrl.Controls.Add(修复)
+        PageCtrl.ItemSize = New Size(1, 25)
+        PageCtrl.Location = New Point(-10, -8)
+        PageCtrl.Name = "PageCtrl"
+        PageCtrl.SelectedIndex = 0
+        PageCtrl.Size = New Size(961, 687)
+        PageCtrl.TabIndex = 4
+        ' 
+        ' MainPgae
+        ' 
+        MainPgae.BackColor = Color.LightSkyBlue
+        MainPgae.Location = New Point(4, 29)
+        MainPgae.Name = "MainPgae"
+        MainPgae.Padding = New Padding(3)
+        MainPgae.Size = New Size(953, 654)
+        MainPgae.TabIndex = 0
+        MainPgae.Text = "主界面"
+        ' 
+        ' MainPage_Scan
+        ' 
+        MainPage_Scan.BackColor = Color.LightSkyBlue
+        MainPage_Scan.Controls.Add(ScanPanel)
+        MainPage_Scan.Controls.Add(Show_ScanPanel)
+        MainPage_Scan.Controls.Add(ScaningPage_Btn)
+        MainPage_Scan.Controls.Add(Tip_0)
+        MainPage_Scan.Location = New Point(4, 29)
+        MainPage_Scan.Name = "MainPage_Scan"
+        MainPage_Scan.Padding = New Padding(3)
+        MainPage_Scan.Size = New Size(953, 654)
+        MainPage_Scan.TabIndex = 1
+        MainPage_Scan.Text = "查杀_主界面"
+        ' 
+        ' ScanPanel
+        ' 
+        ScanPanel.Controls.Add(SelPathScan_Btn)
+        ScanPanel.Location = New Point(144, 386)
+        ScanPanel.Name = "ScanPanel"
+        ScanPanel.Size = New Size(196, 169)
+        ScanPanel.TabIndex = 4
+        ScanPanel.Visible = False
+        ' 
+        ' SelPathScan_Btn
+        ' 
+        SelPathScan_Btn.BackColor = Color.Silver
+        SelPathScan_Btn.Font = New Font("黑体", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        SelPathScan_Btn.Location = New Point(-3, 0)
+        SelPathScan_Btn.Name = "SelPathScan_Btn"
+        SelPathScan_Btn.Size = New Size(199, 63)
+        SelPathScan_Btn.TabIndex = 3
+        SelPathScan_Btn.Text = "自定义查杀"
+        SelPathScan_Btn.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Show_ScanPanel
+        ' 
+        Show_ScanPanel.BackColor = Color.Silver
+        Show_ScanPanel.Font = New Font("黑体", 24F, FontStyle.Regular, GraphicsUnit.Point)
+        Show_ScanPanel.Location = New Point(343, 316)
+        Show_ScanPanel.Name = "Show_ScanPanel"
+        Show_ScanPanel.Size = New Size(43, 67)
+        Show_ScanPanel.TabIndex = 3
+        Show_ScanPanel.Text = "⬇️"
+        Show_ScanPanel.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' ScaningPage_Btn
+        ' 
+        ScaningPage_Btn.BackColor = Color.Silver
+        ScaningPage_Btn.Font = New Font("黑体", 18F, FontStyle.Regular, GraphicsUnit.Point)
+        ScaningPage_Btn.Location = New Point(144, 316)
+        ScaningPage_Btn.Name = "ScaningPage_Btn"
+        ScaningPage_Btn.Size = New Size(196, 67)
+        ScaningPage_Btn.TabIndex = 2
+        ScaningPage_Btn.Text = "全盘查杀"
+        ScaningPage_Btn.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Tip_0
+        ' 
+        Tip_0.AutoSize = True
+        Tip_0.Font = New Font("黑体", 36F, FontStyle.Regular, GraphicsUnit.Point)
+        Tip_0.ForeColor = SystemColors.ButtonHighlight
+        Tip_0.Location = New Point(124, 175)
+        Tip_0.Name = "Tip_0"
+        Tip_0.Size = New Size(535, 120)
+        Tip_0.TabIndex = 1
+        Tip_0.Text = "查杀木马病毒,拦截" & vbCrLf & "可疑行为"
+        ' 
+        ' ScaningPage
+        ' 
+        ScaningPage.BackColor = Color.LightSkyBlue
+        ScaningPage.Controls.Add(VirusCount)
+        ScaningPage.Controls.Add(Del)
+        ScaningPage.Controls.Add(Scan_Tip)
+        ScaningPage.Controls.Add(ListView1)
+        ScaningPage.Location = New Point(4, 29)
+        ScaningPage.Name = "ScaningPage"
+        ScaningPage.Padding = New Padding(3)
+        ScaningPage.Size = New Size(953, 654)
+        ScaningPage.TabIndex = 2
+        ScaningPage.Text = "查杀_扫描中"
+        ' 
+        ' VirusCount
+        ' 
+        VirusCount.AutoSize = True
+        VirusCount.Font = New Font("黑体", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        VirusCount.Location = New Point(9, 46)
+        VirusCount.Name = "VirusCount"
+        VirusCount.Size = New Size(99, 20)
+        VirusCount.TabIndex = 4
+        VirusCount.Text = "威胁数量:"
+        ' 
+        ' Del
+        ' 
+        Del.BackColor = Color.Silver
+        Del.Font = New Font("黑体", 13.8F, FontStyle.Regular, GraphicsUnit.Point)
+        Del.Location = New Point(840, 22)
+        Del.Name = "Del"
+        Del.Size = New Size(94, 41)
+        Del.TabIndex = 3
+        Del.Text = "删除"
+        Del.TextAlign = ContentAlignment.MiddleCenter
+        ' 
+        ' Scan_Tip
+        ' 
+        Scan_Tip.AutoSize = True
+        Scan_Tip.Font = New Font("黑体", 12F, FontStyle.Regular, GraphicsUnit.Point)
+        Scan_Tip.Location = New Point(9, 13)
+        Scan_Tip.Name = "Scan_Tip"
+        Scan_Tip.Size = New Size(99, 20)
+        Scan_Tip.TabIndex = 0
+        Scan_Tip.Text = "正在扫描:"
+        ' 
+        ' ListView1
+        ' 
+        ListView1.BackColor = Color.LightSkyBlue
+        ListView1.Columns.AddRange(New ColumnHeader() {FileName})
+        ListView1.Location = New Point(6, 77)
+        ListView1.Name = "ListView1"
+        ListView1.Size = New Size(941, 577)
+        ListView1.TabIndex = 1
+        ListView1.UseCompatibleStateImageBehavior = False
+        ListView1.View = View.Details
+        ' 
+        ' FileName
+        ' 
+        FileName.Text = "文件名"
+        FileName.Width = 8000
+        ' 
+        ' 修复
+        ' 
+        修复.BackColor = Color.LightSkyBlue
+        修复.Controls.Add(FixTaskMgr)
+        修复.Location = New Point(4, 29)
+        修复.Name = "修复"
+        修复.Padding = New Padding(3)
+        修复.Size = New Size(953, 654)
+        修复.TabIndex = 3
+        修复.Text = "修复"
+        ' 
+        ' FixTaskMgr
+        ' 
+        FixTaskMgr.BackColor = Color.Silver
+        FixTaskMgr.Font = New Font("黑体", 13.5F, FontStyle.Regular, GraphicsUnit.Point)
+        FixTaskMgr.Location = New Point(27, 39)
+        FixTaskMgr.Name = "FixTaskMgr"
+        FixTaskMgr.Size = New Size(196, 49)
+        FixTaskMgr.TabIndex = 9
+        FixTaskMgr.Text = "修复任务管理器"
+        FixTaskMgr.TextAlign = ContentAlignment.MiddleCenter
+        ' 
         ' MainForm
         ' 
         AutoScaleDimensions = New SizeF(9F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.LightSkyBlue
         ClientSize = New Size(1079, 717)
+        Controls.Add(Panel1)
         Controls.Add(CtrlPanel)
-        Controls.Add(PageCtrl)
         Controls.Add(Panel_Move)
         FormBorderStyle = FormBorderStyle.None
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
@@ -251,34 +337,44 @@ Partial Class MainForm
         Text = "Visual Basic Antivirus Software"
         Panel_Move.ResumeLayout(False)
         Panel_Move.PerformLayout()
-        PageCtrl.ResumeLayout(False)
-        MainPage_Scan.ResumeLayout(False)
-        MainPage_Scan.PerformLayout()
-        ScaningPage.ResumeLayout(False)
-        ScaningPage.PerformLayout()
         CtrlPanel.ResumeLayout(False)
         CType(ScanMainPage_Btn, ComponentModel.ISupportInitialize).EndInit()
         CType(MainPage_Btn, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
+        PageCtrl.ResumeLayout(False)
+        MainPage_Scan.ResumeLayout(False)
+        MainPage_Scan.PerformLayout()
+        ScanPanel.ResumeLayout(False)
+        ScaningPage.ResumeLayout(False)
+        ScaningPage.PerformLayout()
+        修复.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Panel_Move As Panel
     Friend WithEvents Btn_Close As Label
     Friend WithEvents Btn_MinWindow As Label
-    Friend WithEvents PageCtrl As TabControl
-    Friend WithEvents MainPgae As TabPage
-    Friend WithEvents MainPage_Scan As TabPage
-    Friend WithEvents Tip_0 As Label
     Friend WithEvents CtrlPanel As Panel
     Friend WithEvents ScanMainPage_Btn As PictureBox
     Friend WithEvents MainPage_Btn As PictureBox
     Friend WithEvents Panel_isGoTo As Panel
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PageCtrl As TabControl
+    Friend WithEvents MainPgae As TabPage
+    Friend WithEvents MainPage_Scan As TabPage
+    Friend WithEvents ScaningPage_Btn As Label
+    Friend WithEvents Tip_0 As Label
     Friend WithEvents ScaningPage As TabPage
+    Friend WithEvents VirusCount As Label
+    Friend WithEvents Del As Label
     Friend WithEvents Scan_Tip As Label
     Friend WithEvents ListView1 As ListView
     Friend WithEvents FileName As ColumnHeader
-    Friend WithEvents ScaningPage_Btn As Label
-    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
-    Friend WithEvents Del As Label
-    Friend WithEvents VirusCount As Label
+    Friend WithEvents Show_ScanPanel As Label
+    Friend WithEvents ScanPanel As Panel
+    Friend WithEvents SelPathScan_Btn As Label
+    Friend WithEvents Fix As Label
+    Friend WithEvents 修复 As TabPage
+    Friend WithEvents FixTaskMgr As Label
 End Class
--- MainForm.vb
+++ MainForm.vb
@@ -1,14 +1,14 @@
 ﻿Imports System.IO
 Imports System.Net.WebRequestMethods
-Imports System.Reflection
 Imports System.Runtime.InteropServices
 Imports System.Security.Cryptography
+Imports System.ServiceProcess
 Imports System.Threading
-Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-Imports System.Xml
 Imports Qihoo.CloudEngine
+Imports Microsoft.Win32
 
 Public Class MainForm
+
     <DllImport("kernel32.dll", CharSet:=CharSet.Unicode)>
     Shared Function LoadLibrary(lpLibFileName As String) As IntPtr
     End Function
@@ -25,6 +25,7 @@ Public Class MainForm
     Public Const WM_NCLBUTTONDOWN = &HA1
     Public NSudoPath = Application.StartupPath + "NSudoDM.dll"
     Private Declare Sub Sleep Lib "kernel32" (ByVal dwMilliseconds As Long)
+    Public IsShow_ScanPanel
     'Public State As Int16 = 0
 
     Private Sub Panel_Move_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel_Move.MouseDown
@@ -33,6 +34,13 @@ Public Class MainForm
     End Sub
 
     Private Sub Btn_Close_Click(sender As Object, e As EventArgs) Handles Btn_Close.Click
+        For i = 0 To 20
+            Width -= 20
+            Height -= 20
+            Left += 10
+            Top += 10
+            Opacity -= 0.22
+        Next
         Application.Exit()
     End Sub
 
@@ -40,6 +48,7 @@ Public Class MainForm
         MinWindow()
     End Sub
     Public Sub MinWindow()
+
         WindowState = FormWindowState.Minimized
     End Sub
     Public Sub GoToPage(index As Int32)
@@ -50,6 +59,9 @@ Public Class MainForm
             GoToPageAnimation(Panel_isGoTo, ScanMainPage_Btn)
             PageCtrl.SelectedIndex = index - 1
         ElseIf index = 3 Then
+            PageCtrl.SelectedIndex = index - 1
+        ElseIf index = 4 Then
+            GoToPageAnimation(Panel_isGoTo, Fix)
             PageCtrl.SelectedIndex = index - 1
         End If
     End Sub
@@ -71,7 +83,7 @@ Public Class MainForm
         GoToPage(2)
     End Sub
 
-    Private Sub ScaningPage_Btn_Click(sender As Object, e As EventArgs)
+    Private Sub _ScaningPage_Btn_Click(sender As Object, e As EventArgs)
 
     End Sub
     Public Async Function Scan(ByVal directoryPath As String) As Task
@@ -124,35 +136,21 @@ Public Class MainForm
                 MsgBox("扫描失败！")
             End If
 
-
+            FreeLibrary(NSudoDevilModeModuleHandle)
             For Each subDir In Directory.GetDirectories(directoryPath)
                 Await Scan(subDir)
             Next
-            FreeLibrary(NSudoDevilModeModuleHandle)
+
         Catch e As Exception
             Debug.Print(e.Message)
         End Try
         'End If
     End Function
 
-    Private Async Sub ScaningPage_Btn_Click_1(sender As Object, e As EventArgs) Handles ScaningPage_Btn.Click
-        ListView1.Items.Clear()
-        Me.VirusCount.Text = "威胁数量:0"
-        Me.Scan_Tip.Text = "正在扫描:"
 
-        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
 
-            '  State = 0
-            GoToPage(3)
-            Dim s As New Scan
 
-            Await Scan(FolderBrowserDialog1.SelectedPath)
-
-        End If
-
-    End Sub
-
-    Private Sub Del_Click(sender As Object, e As EventArgs) Handles Del.Click
+    Private Sub Del_Click(sender As Object, e As EventArgs)
         Dim NSudoPath = Application.StartupPath + "NSudoDM.dll"
 
         Dim NSudoDevilModeModuleHandle As IntPtr = LoadLibrary(NSudoPath) ' DLL文件名  
@@ -173,7 +171,7 @@ Public Class MainForm
 
     End Sub
 
-    Private Sub MainPgae_Click(sender As Object, e As EventArgs) Handles MainPgae.Click
+    Private Sub MainPgae_Click(sender As Object, e As EventArgs)
 
     End Sub
 
@@ -181,14 +179,93 @@ Public Class MainForm
         PageCtrl.ItemSize = New Size(1, 1)
         PageCtrl.SizeMode = TabSizeMode.Fixed
         PageCtrl.Appearance = TabAppearance.FlatButtons
-        ' ActiveDefense.Show()
+
+        Process.Start(Application.StartupPath & "\VBASTray.exe")
+
+        Panel1.Margin = New Padding(0, 0, 0, 0)
+
+    End Sub
+
+    Private Async Sub ScaningPage_Btn_Click(sender As Object, e As EventArgs) Handles ScaningPage_Btn.Click
+        ListView1.Items.Clear()
+        Me.VirusCount.Text = "威胁数量:0"
+        Me.Scan_Tip.Text = "正在扫描"
+        GoToPage(3)
+        Await Scan("C:\")
+
+
     End Sub
 
 
+
+    Private Sub MainPage_Scan_Click(sender As Object, e As EventArgs) Handles MainPage_Scan.Click
+
+    End Sub
+
+
+
+    Private Async Sub SelPathScan_Btn_Click(sender As Object, e As EventArgs) Handles SelPathScan_Btn.Click
+        ListView1.Items.Clear()
+        Me.VirusCount.Text = "威胁数量:0"
+        Me.Scan_Tip.Text = "正在扫描"
+
+
+
+        If FolderBrowserDialog1.ShowDialog = DialogResult.OK Then
+
+            '  State = 0
+            GoToPage(3)
+            '  Dim s As New Scan
+
+            Await Scan(FolderBrowserDialog1.SelectedPath)
+
+        End If
+    End Sub
+
+    Private Sub Show_ScanPanel_Click_1(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub Panel_Move_Paint(sender As Object, e As PaintEventArgs) Handles Panel_Move.Paint
+
+    End Sub
+
+    Private Sub Show_ScanPanel_Click(sender As Object, e As EventArgs)
+
+
+    End Sub
+
+    Private Sub Tip_0_Click(sender As Object, e As EventArgs) Handles Tip_0.Click
+
+    End Sub
+
+    Private Sub Show_ScanPanel_Click_2(sender As Object, e As EventArgs) Handles Show_ScanPanel.Click
+        If ScanPanel.Visible Then
+            ScanPanel.Visible = False
+            Show_ScanPanel.Text = "⬇"
+        Else
+            ScanPanel.Visible = True
+            Show_ScanPanel.Text = "⬆️"
+        End If
+    End Sub
+
+    Private Sub Fix_Click(sender As Object, e As EventArgs) Handles Fix.Click
+        GoToPage(4)
+    End Sub
+
+    Private Sub FixTaskMgr_Click(sender As Object, e As EventArgs) Handles FixTaskMgr.Click
+        Dim registryKey As RegistryKey = Registry.CurrentUser.CreateSubKey("Software\Microsoft\Windows\CurrentVersion\Policies\System")
+        If registryKey IsNot Nothing Then
+            registryKey.SetValue("DisableTaskMgr", 0)
+            registryKey.Close()
+
+        End If
+    End Sub
 End Class
 
 Public Class Scan
-
+    '已废弃
     <DllImport("kernel32.dll", CharSet:=CharSet.Unicode)>
     Shared Function LoadLibrary(lpLibFileName As String) As IntPtr
     End Function
@@ -196,12 +273,12 @@ Public Class Scan
     <DllImport("kernel32.dll", SetLastError:=True)>
     Shared Function FreeLibrary(hLibModule As IntPtr) As Boolean
     End Function
-    Public yes As Boolean = True
+
     Public Async Function ProcessCheck() As Task
-        '半成品，bug还没修好，别用
 
 
-        Await Task.Run(Sub()
+
+        Await Task.Run(Async Function()
                            Dim NSudoPath = Application.StartupPath + "NSudoDM.dll" 'dll路径
                            Dim prevProcesses() As Process = {}
                            Dim currentProcesses() As Process = Process.GetProcesses()
@@ -214,32 +291,65 @@ Public Class Scan
                                ' 检测新进程  
                                For Each newProcess In currentProcessesSnapshot.Except(prevProcesses).ToList
                                    Try
-                                       Dim md5 As String = s.getMD5(newProcess.MainModule.FileName)
+                                       Dim md5 As String
+                                       If Not (IsServiceRunning(newProcess.ProcessName)) Then
+                                           md5 = s.getMD5(newProcess.MainModule.FileName)
+                                       Else
+                                           Continue For
+                                       End If
 
                                        Dim result As FileHealthResult = FileHealth.CheckAsync(md5).Result
-                                       If result IsNot Nothing Then
-                                           If result.IsOperationSuccess Then
-                                               If CInt(result.Level) > 50 Then
-                                                   Debug.Print(result.Level)
+                                       If result IsNot Nothing And result.IsOperationSuccess Then
+
+                                           If result.Level > 50 Then
+                                               If MainForm.InvokeRequired Then
+                                                   MainForm.Invoke(Sub() start(newProcess.MainModule.FileName, result.MalwareType, newProcess.ProcessName))
                                                Else
-                                                   Debug.Print(result.Level)
+                                                   start(newProcess.MainModule.FileName, result.MalwareType, newProcess.ProcessName)
                                                End If
+
                                            End If
+                                           Debug.Print("Name=" & newProcess.ProcessName & ",Path=" & newProcess.MainModule.FileName & ",Level=" & result.Level)
+
                                        End If
                                        prevProcesses.Append(newProcess)
                                    Catch ex As Exception
-
+                                       Debug.Print(ex.Message)
                                    End Try
                                Next
 
                                ' 更新prevProcesses列表  
                                prevProcesses = currentProcessesSnapshot
                                FreeLibrary(NSudoDevilModeModuleHandle)
-                               Thread.Sleep(100)
+
 
                            End While
-                       End Sub)
+                       End Function)
     End Function
+    Public Function start(a, __, ___)
+        Dim ad As New ActiveDefense()
+        ad.Show()
+        ' ad.Receive_Messages(a, __, ___)
+        start = ""
+    End Function
+    Function IsServiceRunning(serviceName As String) As Boolean
+        Try
+
+            Using serviceController As New ServiceController(serviceName)
+                Return serviceController.Status = ServiceControllerStatus.Running
+            End Using
+        Catch ex As Exception When (TypeOf ex Is InvalidOperationException)
+            ' 服务可能不存在，忽略异常并返回False  
+            Console.WriteLine("The service does not exist or is not accessible.")
+            Return False
+        Catch ex As Exception
+            ' 处理其他异常  
+            Console.WriteLine("An error occurred while checking the service status: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+
     Public Function getMD5(ByVal strSource As String) As String
         Dim r As String = ""
         ' If MainForm.State = 0 Then
